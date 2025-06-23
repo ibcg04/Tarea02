af1c40a68da5c8547f7847b3bd885826858acf8e
Sebastian B <sgasparbarco@gmail.com>
2025-06-22 19:26:10 -0500

    Refactoring de metodos en Huesped

--- proyectods/src/main/java/ec/edu/espol/Huesped.java
+++ proyectods/src/main/java/ec/edu/espol/Huesped.java
@@ -5,21 +5,10 @@ import java.util.ArrayList;
 public class Huesped extends Usuario {
     private Unidad unidadOcupada;
 
-    public Reseña reseñar(){
-        return new Reseña();
-    }
-    public void mostrarPropiedades(ArrayList<Propiedad> propiedades){
-        System.out.println("Propiedades disponibles:");
-        
-        //for(Propiedad p: propiedades){
-        //    System.out.println(p);
-        //}
-    }
-
     public ArrayList<Propiedad> buscarPropiedades(String ubicacion, int calificacion){
         //Busca propiedades en la ubicación y con la reseña.
         ArrayList<Propiedad> propiedades_nuevas = new ArrayList<>();
-        System.out.println("Buscando propiedades...");
+        System.out.println("Buscando propiedades.....");
         for(Anfitrion a: DataBase.getDataBase().getAnfitriones()){
             for(Propiedad p: a.getPropiedades()){
                 if(p.getUbicacion().equals(ubicacion)){
@@ -33,6 +22,18 @@ public class Huesped extends Usuario {
         mostrarPropiedades(propiedades_nuevas); 
         }
         return propiedades_nuevas;
+    }
+
+      public Reseña reseñar(){
+        return new Reseña();
+    }
+
+     public void mostrarPropiedades(ArrayList<Propiedad> propiedades){
+        System.out.println("Propiedades disponibles:");
+        
+        //for(Propiedad p: propiedades){
+        //    System.out.println(p);
+        //}
     }
 
 }
