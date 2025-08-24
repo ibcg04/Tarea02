91d625c8691fab9dec603cbded35525e67045d29
Caluloper <carlosluislopezcba@gmail.com>
2025-08-23 23:21:27 -0500

    Detalles

--- proyectods/src/test/java/util/EntradaUtilsTest.java
+++ proyectods/src/test/java/util/EntradaUtilsTest.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import utilidades.EntradaUtils;
 
 class EntradaUtilsTest {
 
     @Test
+    @DisplayName("Entrada válida dentro de rango retorna el valor ingresado")
     void testEntradaValidaDentroDeRango() {
         String input = "5\n";
         Scanner sc = new Scanner(new ByteArrayInputStream(input.getBytes()));
@@ -19,6 +21,7 @@ class EntradaUtilsTest {
     }
 
     @Test
+    @DisplayName("Entrada fuera de rango seguida de válida retorna la segunda entrada")
     void testEntradaFueraDeRangoLuegoValida() {
         String input = "15\n7\n";
         Scanner sc = new Scanner(new ByteArrayInputStream(input.getBytes()));
@@ -27,6 +30,7 @@ class EntradaUtilsTest {
     }
 
     @Test
+    @DisplayName("Entrada no entera seguida de válida retorna la segunda entrada")
     void testEntradaNoEnteraLuegoValida() {
         String input = "abc\n3\n";
         Scanner sc = new Scanner(new ByteArrayInputStream(input.getBytes()));
@@ -35,6 +39,7 @@ class EntradaUtilsTest {
     }
 
     @Test
+    @DisplayName("Entrada en el límite inferior retorna el valor mínimo")
     void testEntradaEnElLimiteInferior() {
         String input = "1\n";
         Scanner sc = new Scanner(new ByteArrayInputStream(input.getBytes()));
@@ -43,6 +48,7 @@ class EntradaUtilsTest {
     }
 
     @Test
+    @DisplayName("Entrada en el límite superior retorna el valor máximo")
     void testEntradaEnElLimiteSuperior() {
         String input = "10\n";
         Scanner sc = new Scanner(new ByteArrayInputStream(input.getBytes()));
